91d1a51f4366e7417bc0289f8afbd1621f92fdf9
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-02-08 11:12:32 -0500

    Finished Lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = (num1 * num2)
+let random = Math.floor( Math.random() * (10000))
+
+let num3 = 34
+let num4 = 10
+let mod = (num3 % num4)
+let max = Math.max(1, 2, 5, 6, 4, 9, 15, 20)
